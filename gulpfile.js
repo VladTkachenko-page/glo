ac64745f92b4da6417cc4235a6568cc58e1de051
Vlad Tkachenko <vladtka@ukr.net>
2020-05-06 14:39:08 +0300

    feat: add gulp-autoprefixer in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const browserSync = require('browser-sync').create();
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 function bs() {
   serveSass();
@@ -19,6 +20,9 @@ function bs() {
 function serveSass() {
   return src("./sass/*.sass")
     .pipe(sass())
+    .pipe(autoprefixer({
+      cascade: false
+  }))
     .pipe(dest("./css"))
     .pipe(browserSync.stream())
 };
@@ -29,4 +33,5 @@ function minCSS() {
   .pipe(cleanCSS())
   .pipe(dest("./css"));
 };
+
 exports.serve = bs;
