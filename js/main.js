74e0712ad2169c8f2bdf7e7ecf7acd027d28083b
Vlad Tkachenko <vladtka@ukr.net>
2020-05-14 12:54:47 +0300

    refact: remove js code in main.js

--- js/main.js
+++ js/main.js
@@ -1,35 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function(event) { 
-//   const modal = document.querySelector('.modal');
-//   const modalDialog = document.querySelector('.modal__dialog')
-//   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-//   const closeBtn = document.querySelector('.modal__close');
-//   const switchModal = () => {
-//     modal.classList.toggle('modal--visible');
-//   }
-
-//   modalBtn.forEach(element => {
-//     element.addEventListener('click', switchModal);
-//   });
-  
-//   closeBtn.addEventListener('click', switchModal);
-  
-//   modal.addEventListener('click', event => {
-//     const isClickInside = modalDialog.contains(event.target);
-
-//     if (!isClickInside) {
-//       modal.classList.toggle('modal--visible');
-//     }
-//   })
-
-// });
-// document.body.addEventListener('keyup', (e) => {
-//   const key = e.keyCode;
-
-//   if (key == 27) {
-//       document.querySelector('.modal--visible').classList.remove('modal--visible');
-//   };
-// });
-
 $(document).ready(function () {
   var modal = $('.modal'),
       modalDialog = $('.modal__dialog'),
