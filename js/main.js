2d754441f4fae81734f4c128bd79bfcc1399ce24
Vlad Tkachenko <vladtka@ukr.net>
2020-05-15 15:32:17 +0300

    feat: add section projects and slider in this section and make pixel perfect

--- js/main.js
+++ js/main.js
@@ -13,22 +13,22 @@ $(document).ready(function () {
     modal.toggleClass('modal--visible');
   });
   
-  modal.mouseup(function (e) {
-    if (modalDialog.has(e.target).length === 0){
+  modal.on('click', function (e) {
+    if (e.target!=modalDialog[0]&&!modalDialog.has(e.target).length){
         modal.toggleClass('modal--visible');  
     }
 });
   
-  $(this).keydown(function(eventObject){
+  $(document).keydown(function(eventObject){
     if (eventObject.which == 27)
     $('.modal--visible').removeClass('modal--visible');
     });
   
-    $(window).scroll(function(){
-      if($(window).scrollTop() > 120) {
-        $('.scroll-up').show();
+  $(window).scroll(function(){
+    if($(window).scrollTop() > 120) {
+      $('.scroll-up').show();
       } else {
-        $('.scroll-up').hide();
+      $('.scroll-up').hide();
       }
     });
   
@@ -36,4 +36,25 @@ $(document).ready(function () {
     $('body, html').animate({scrollTop: 0}, 1000);
   });
 
+  $('.hero__scroll-down').on("click", "", function() {
+    $('html').animate({scrollTop: $('.control').position().top - 80}, 1000);
+  });
+
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+  });
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() + 15 + bullets.width() + 15);
+  bullets.css('left', prev.width() + 15)
 });
